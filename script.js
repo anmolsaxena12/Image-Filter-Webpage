32e060a3c3c4751f5765270d7e6903d93a3d1e9c
anmolsaxena12 <66118169+anmolsaxena12@users.noreply.github.com>
2020-06-30 14:25:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+var image;
+var Oimg;
+
+function upload() {
+  var fileinput =                    document.getElementById('finput');
+  var canvas = document.getElementById('imgcan');
+  image = new SimpleImage(fileinput);
+  Oimg = new SimpleImage('image');
+  image.drawTo(canvas);
+}
+function clear(){
+  var canvasop = document.getElementById('canop');
+  ctx = canvasop.getContext("2d");
+  context.clearRect(0, 0, 500, 500);
+}
+
+function toGray(){
+  for(var px of image.values()){
+    avg = (px.getRed()+px.getBlue()+px.getGreen())/3;
+    px.setRed(avg);
+    px.setBlue(avg);
+    px.setGreen(avg);
+  }
+  var canvasop = document.getElementById('canop');
+    image.drawTo(canvasop);
+}
+function toYellow(){
+  for(var px of image.values()){
+    var x = px.getRed();
+    var y=px.getBlue();
+    var z = px.getGreen();
+    px.setRed(x+20);
+    px.setBlue(y);
+    px.setGreen(z+20);
+  }
+  var canvasop = document.getElementById('canop');
+    image.drawTo(canvasop);
+}
+function toRed(){
+  for(var px of image.values()){
+    var x = px.getRed();
+    var y=px.getBlue();
+    var z = px.getGreen();
+    px.setRed(x+20);
+    px.setBlue(y);
+    px.setGreen(z);
+  }
+  var canvasop = document.getElementById('canop');
+    image.drawTo(canvasop);
+}
+function toBlue(){
+  for(var px of image.values()){
+    var x = px.getRed();
+    var y=px.getBlue();
+    var z = px.getGreen();
+    px.setRed(x);
+    px.setBlue(y+20);
+    px.setGreen(z);
+  }
+  var canvasop = document.getElementById('canop');
+    image.drawTo(canvasop);
+}
